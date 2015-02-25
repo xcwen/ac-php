40580d3af4789dd2004173876e11842c03643ea9
jim <xcwenn@qq.com>
2015-02-25 17:33:18 +0800

    update version

--- ac-php.el
+++ ac-php.el
@@ -5,8 +5,8 @@
 
 ;; Author: xcwenn@qq.com [https://github.com/xcwen]
 ;; URL: https://github.com/xcwen/ac-php
-;; Keywords: completion, convenience, intellisense 
-;; Version: 20150224
+;; Keywords: completion, convenience, intellisense
+;; Version: 20150225
 ;; Package-Requires: ((emacs "24") ( php-mode "1") (auto-complete "1.4.0") (yasnippet "0.8.0"))
 
 

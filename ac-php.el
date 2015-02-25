e73c44928f8154e4ab052dc682db47fc70edcc21
jim <xcwenn@qq.com>
2015-02-25 12:29:10 +0800

    fix doc

--- ac-php.el
+++ ac-php.el
@@ -30,6 +30,16 @@
 ;; Auto Completion source for php. 
 ;; Only support  Linux , not support Windows
 ;; More info and **example** at : https://github.com/xcwen/ac-php 
+;; add to .emacs:
+;; 
+;;(add-hook 'php-mode-hook '(lambda ()
+;;                            (auto-complete-mode t)
+;;                            (require 'ac-php)
+;;                            (setq ac-sources  '(ac-source-php ) )
+;;							(yas-global-mode 1)
+;;							(define-key php-mode-map  (kbd "C-]") 'ac-php-find-symbol-at-point)   ;goto define
+;;							(define-key php-mode-map  (kbd "C-t") 'ac-php-location-stack-back   ) ;go back
+;;							))
 ;; 
 
 ;;; Code:

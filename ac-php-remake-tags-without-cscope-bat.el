38c1de74d1c23ac4c653fb29ec362f697decabbb
xcwen <xcwenn@qq.com>
2015-05-24 12:18:59 +0800

    fix bat

--- ac-php-remake-tags-without-cscope-bat.el
+++ ac-php-remake-tags-without-cscope-bat.el
@@ -2,7 +2,9 @@
 ;; emacs -Q --script ~/.emacs.d/elpa/ac-php-*/ac-php-remake-tags-without-cscope-bat.el 
 (require 'package)
 (package-initialize)
+(setq load-path (append  load-path  (list (file-name-directory load-file-name)) ))
 (require 'ac-php)
 (setq ac-php-cscope nil)
 (ac-php-remake-tags)
+
 
--- ac-php.el
+++ ac-php.el
@@ -1,12 +1,10 @@
 ;;; ac-php.el --- Auto Completion source for php for GNU Emacs
 
-;; Copyright (C) 2014  jim 
-
+;; Copyright (C) 2014 - 2015 jim 
 
 ;; Author: xcwenn@qq.com [https://github.com/xcwen]
 ;; URL: https://github.com/xcwen/ac-php
 ;; Keywords: completion, convenience, intellisense
-;; Version: 20150226
 ;; Package-Requires: ((emacs "24") ( php-mode "1") (auto-complete "1.4.0") (yasnippet "0.8.0") (xcscope "1") (s "1") (f "1"))
 
 

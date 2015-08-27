8507ef62bb598eab79c7f9f007629c6c5af4637c
xcwen <xcwenn@qq.com>
2015-08-27 09:53:25 +0800

    fix

--- cp_to_elpa.sh
+++ cp_to_elpa.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd `dirname $0`
-rm -rf ~/.emacs.d/elpa/ac-php-2015*/*.elc
-cp -rf *.el phpctags ~/.emacs.d/elpa/ac-php-2015*/
+rm -rf ~/.emacs.d/elpa/ac-php-20*/*.elc
+cp -rf *.el phpctags ~/.emacs.d/elpa/ac-php-20*/

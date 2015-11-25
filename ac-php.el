88c326927ffbd7d3f6e240b411b170ec7e2ed071
xcwen <xcwenn@qq.com>
2015-11-25 10:59:09 +0800

    fix require

--- ac-php.el
+++ ac-php.el
@@ -5,7 +5,7 @@
 ;; Author: xcwenn@qq.com [https://github.com/xcwen]
 ;; URL: https://github.com/xcwen/ac-php
 ;; Keywords: completion, convenience, intellisense
-;; Package-Requires: ((emacs "24") ( php-mode "1") (auto-complete "1.4.0") (yasnippet "0.8.0") (xcscope "1") (s "1") (f "1") (popup "1")) 
+;; Package-Requires: ((emacs "24") ( php-mode "1") (auto-complete "1.4.0") (yasnippet "0.8.0") (xcscope "1") (s "1") (f "1") (popup "1") (company "1" )) 
 
 
 

48f1907d41cb29c1fddb77031027614cbe65b8fa
xcwen <xcwenn@qq.com>
2016-01-08 11:31:27 +0800

    add isset unset empty function

---
+++ commom_php/__sys.el
@@ -0,0 +1,5 @@
+("empty"  "/home/jim/ac-php/commom_php/func.php:6"  " /** @return bool */ function empty( mixed $var ){}"  "f"  ()  "bool" )
+("isset"  "/home/jim/ac-php/commom_php/func.php:6"  " /** @return bool */ function isset( mixed $var , mixed $__args__=NULL   ){} "  "f"  ()  "bool" )
+("unset"  "/home/jim/ac-php/commom_php/func.php:6"  " /** @return void */ function unset( mixed $var , mixed $__args__=NULL   ){}"  "f"  ()  "void" )
+
+
---
+++ commom_php/cp_sys_el.sh
@@ -0,0 +1,1 @@
+cp ./__sys.el ~/.ac-php/tags-home-jim-ac-php-commom_php/tags_dir_jim/

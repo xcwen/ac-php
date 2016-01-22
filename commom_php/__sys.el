d34b6aebaf4d85f036a2529aa689afdde9983bef
xcwen <xcwenn@qq.com>
2016-01-22 19:26:08 +0800

    add include_once

--- commom_php/__sys.el
+++ commom_php/__sys.el
@@ -4,5 +4,6 @@
 ("require"  "/home/jim/ac-php/commom_php/func.php:6"  " /** @return mixed*/ function require(string $file_name ){}"  "f"  ()  "mixed" )
 ("require_once"  "/home/jim/ac-php/commom_php/func.php:6"  " /** @return mixed*/ function require_once(string $file_name ){}"  "f"  ()  "mixed" )
 ("include"  "/home/jim/ac-php/commom_php/func.php:6"  " /** @return mixed */ function include(string $file_name ){}"  "f"  ()  "mixed" )
+("include_once"  "/home/jim/ac-php/commom_php/func.php:6"  " /** @return mixed */ function include_once(string $file_name ){}"  "f"  ()  "mixed" )
 
 

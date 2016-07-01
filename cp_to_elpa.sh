a9edb3f7056ebb1aa612bbc0045ec2c677890e98
jim <xcwenn@qq.com>
2016-07-01 11:48:30 +0800

    fix error gen tags

--- cp_to_elpa.sh
+++ cp_to_elpa.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd `dirname $0`
 rm -rf ~/.emacs.d/elpa/ac-php-20*/*.elc
-cp -rf *.el phpctags ~/.emacs.d/elpa/ac-php-20*/
-chmod a-w ~/.emacs.d/elpa/ac-php-20*/*.el
+cp -rf ac-php-core.el phpctags ~/.emacs.d/elpa/ac-php-core-20*/
+chmod a-w ~/.emacs.d/elpa/ac-php-core-20*/*.el

2cdcc10f7ca442f23a431f551dc6471704070d04
xcwen <xcwenn@qq.com>
2017-11-26 23:35:46 +0800

    f

--- cp_to_elpa.sh
+++ cp_to_elpa.sh
@@ -3,7 +3,7 @@ cd `dirname $0`
 rm -rf ~/.emacs.d/elpa/ac-php-20*/*.elc
 rm -rf ~/.emacs.d/elpa/ac-php-core-20*/*.elc
 rm -rf ~/.emacs.d/elpa/company-php-20*/*.elc
-cp -rf ac-php-core.el phpctags ac-php-comm-tags-data.json ~/.emacs.d/elpa/ac-php-core-20*/
+cp -rf ac-php-core.el phpctags ac-php-comm-tags-data.* ~/.emacs.d/elpa/ac-php-core-20*/
 cp -rf company-php.el ~/.emacs.d/elpa/company-php-20*/
 
 chmod a-w ~/.emacs.d/elpa/ac-php-core-20*/*.el
@@ -12,7 +12,7 @@ chmod a-w ~/.emacs.d/elpa/ac-php-core-20*/*.el
 rm -rf ~/.emacs.d/elpa/develop/ac-php-20*/*.elc
 rm -rf ~/.emacs.d/elpa/develop/ac-php-core-20*/*.elc
 rm -rf ~/.emacs.d/elpa/develop/company-php-20*/*.elc
-cp -rf ac-php-core.el phpctags ac-php-comm-tags-data.json ~/.emacs.d/elpa/develop/ac-php-core-20*/
+cp -rf ac-php-core.el phpctags ac-php-comm-tags-data.* ~/.emacs.d/elpa/develop/ac-php-core-20*/
 cp -rf company-php.el ~/.emacs.d/elpa/develop/company-php-20*/
 
 chmod a-w ~/.emacs.d/elpa/develop/ac-php-core-20*/*.el

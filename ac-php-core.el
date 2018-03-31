2b4c30518cf60bba04f83f0e9d7be5737372db46
firstrow <firstrow@gmail.com>
2018-03-31 21:00:41 +0200

    Import classes using popup

--- ac-php-core.el
+++ ac-php-core.el
@@ -2186,6 +2186,26 @@ Set this variable to nil to disable the lighter."
     )
   )
 
+(defun ac-php-add-use ()
+  (interactive)
+  (save-excursion
+    (ac-php-goto-use)
+    (end-of-line)
+    (newline)
+    ;; get rid of leading slash and insert use statement
+    (insert (format "use %s;" (subseq (ac-php-classes-popup) 1)))))
+
+(defun ac-php-classes-popup ()
+  (ivy-completing-read "Enter class name: " (ac-php-g--class-map (ac-php-get-tags-data)) nil nil ""))
+
+(defun ac-php-goto-use ()
+  (interactive)
+  (xref-push-marker-stack)
+  (goto-char (point-min))
+  ;; try to find first "namespace" keyword to add new "use"s after it.
+  (re-search-forward "^namespace [a-zA-Z0-9\].*;$" nil t)
+  ;; find last "use" keyword.
+  (while (re-search-forward "^use [a-zA-Z0-9\].*;$" nil t)))
 
 ;;;###autoload
 (defun ac-php-core-eldoc-setup ()
